3f1c116cbde8c72cbcb199ee3ccb5250bf3980a4
RubenVG <rmvang01@gmail.com>
2023-05-07 23:32:21 +0200

    #3 created toast message component

---
+++ resources/js/Components/ToastMessage.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { useToast } from "vue-toastification";
+// Import the CSS or use your own!
+import "vue-toastification/dist/index.css";
+
+defineProps({
+    value: String,
+    type: String,
+});
+
+// Get toast interface
+const toast = useToast();
+
+// or with options
+toast.success("My toast content", {
+        timeout: 2000
+      });
+
+
+</script>
+
+<template>
+</template>
