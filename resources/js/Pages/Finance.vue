d0a1a692806ac1ae3bd3c18b77993e8d9f868b32
RubenVG <rmvang01@gmail.com>
2023-05-09 23:44:56 +0200

    #3 added computed property to allow for real time updates

--- resources/js/Pages/Finance.vue
+++ resources/js/Pages/Finance.vue
@@ -1,28 +1,40 @@
 <script setup>
     import AppLayout from '@/Layouts/AppLayout.vue';
-    import Welcome from '@/Components/Welcome.vue';
     import FinanceList from '@/Components/FinanceList.vue';
-    import { defineProps } from 'vue';
+    import ToastMessage from '@/Components/ToastMessage.vue';
+    import { computed, onMounted, ref } from 'vue';
 
     const props = defineProps({
         records: {
             type: Array,
             required: true,
         },
+        columns: {
+            type: Array,
+            required: true,
+        },
+        toast: {
+            type: Array,
+            required: false,
+        }
     });
 
-    const records = props.records;
+    const records = computed(() => {
+        return props.records;
+    })
 
-    console.log(records.id);
+    const toast = computed(() => {
+        return props.toast;
+    })
 
-    // headers = records[0].keys();
-    // console.log(headers);
+    const columns = props.columns;
 
 </script>
 
 <template>
     <AppLayout title="Finance">
         <template #header>
+            <ToastMessage v-if="toast" :message="toast[1]" :severity="toast[0]"></ToastMessage>
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
                 Finance test view
             </h2>
@@ -31,7 +43,7 @@
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                    <FinanceList :records="records" />
+                    <FinanceList :columns="columns" :records="records" />
                 </div>
             </div>
         </div>
