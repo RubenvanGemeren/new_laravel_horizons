ec6acec851c46dc70f7be8222d970525f194d436
RubenVG <rmvang01@gmail.com>
2023-05-16 23:36:37 +0200

    #3 updated dropdown to send data properly

--- resources/js/Components/SelectDropdown.vue
+++ resources/js/Components/SelectDropdown.vue
@@ -1,32 +1,38 @@
 <script setup>
 import { onMounted, ref } from 'vue';
 
-defineProps({
-    modelValue: String,
-    options: Array,
+const props = defineProps({
+  modelValue: String,
+  options: Array,
 });
-
-defineEmits(['update:modelValue']);
 
 const input = ref(null);
 
 onMounted(() => {
-    if (input.value.hasAttribute('autofocus')) {
-        input.value.focus();
-    }
+  if (input.value && input.value.hasAttribute('autofocus')) {
+    input.value.focus();
+  }
 });
 
-defineExpose({ focus: () => input.value.focus() });
+const emit = defineEmits(['update:modelValue']);
+
+const emitUpdate = (value) => {
+  const inputValue = value.target.value;
+  emit('update:modelValue', inputValue);
+};
+
+
+defineExpose({ focus: () => input.value && input.value.focus() });
 </script>
 
 <template>
-    <select
-        ref="input"
-        class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
-        @input="$emit('update:modelValue', $event.target.value)"
-    >
-        <option disabled selected value="">Please select one</option>
-        <option v-for="option in options" :value="option">{{ option }}</option>
-
-    </select>
+  <select
+    ref="input"
+    class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
+    v-model="props.modelValue"
+    @change="emitUpdate"
+  >
+    <option disabled value="">Please select one</option>
+    <option v-for="option in props.options" :value="option">{{ option }}</option>
+  </select>
 </template>
