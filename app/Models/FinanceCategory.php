155ff02db806bf78ca85b865da4605f25a4c9200
RubenVG <rmvang01@gmail.com>
2023-05-23 23:51:37 +0200

    #3 #4 added laravel model, controller, migration for categories

---
+++ app/Models/FinanceCategory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FinanceCategory extends Model
+{
+    use HasFactory;
+
+    protected $table = 'finance_categories';
+
+    protected $guarded = [
+        //
+    ];
+}
