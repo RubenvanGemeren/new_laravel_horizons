ca5e03b979c8a8d3d93683fd25641019a37d65a3
RubenVG <rmvang01@gmail.com>
2023-05-26 18:20:55 +0200

    #4 #3 add finance category controller

--- app/Http/Controllers/FinanceCategoryController.php
+++ app/Http/Controllers/FinanceCategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\FinanceCategory;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class FinanceCategoryController extends Controller
 {
@@ -12,7 +13,23 @@ class FinanceCategoryController extends Controller
      */
     public function index()
     {
-        //
+        $dataTableConfig = [
+            ['text' => "#", 'value' => 'id', 'sortable' => false],
+            ['text' => "Name", 'value' => 'name', 'sortable' => true],
+            ['text' => "Description", 'value' => 'description', 'sortable' => true],
+            ['text' => "Color", 'value' => 'color', 'sortable' => true],
+        ];
+
+        $databaseColumns = array_map(fn ($item) => $item['value'], $dataTableConfig);
+
+        array_push($dataTableConfig, ['text' => "Operation", 'value' => "operation", 'width' => 20]);
+
+        // return $databaseColumns;
+
+        return Inertia::render('Category', [
+            'records' => FinanceCategory::select($databaseColumns)->get(),
+            'columns' => $dataTableConfig,
+        ]);
     }
 
     /**
@@ -28,7 +45,26 @@ class FinanceCategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $validated = $request->validate([
+                'name' => ['required', 'max:200'],
+                'description' => ['nullable', 'max:500'],
+                'color' => ['required', 'max:9', 'starts_with:#'],
+            ]);
+
+            // Create record
+            $record = FinanceCategory::updateOrCreate($validated);
+
+            $request->session()->flash('toast', ['success', 'Category ' . '"' . $record->name. '"' . ' has been added!', time()]);
+
+            return redirect()->back();
+
+        } catch (\Throwable $th) {
+
+            $request->session()->flash('toast', ['warning', $th->getMessage(), time()]);
+
+            return redirect()->back();
+        }
     }
 
     /**
@@ -50,16 +86,48 @@ class FinanceCategoryController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, FinanceCategory $financeCategory)
+    public function update(Request $request, $recordId)
     {
-        //
+        try {
+            $validated = $request->validate([
+                'name' => ['required', 'max:200'],
+                'description' => ['nullable', 'max:500'],
+                'color' => ['required', 'max:9', 'starts_with:#'],
+            ]);
+
+            // Update record
+            FinanceCategory::find($recordId)->update($validated);
+
+            $request->session()->flash('toast', ['success', 'The category ' . '"' . $validated['name']. '"' . ' has been updated!', time()]);
+
+            return redirect()->back();
+
+        } catch (\Throwable $th) {
+
+            $request->session()->flash('toast', ['warning', $th->getMessage(), time()]);
+
+            return redirect()->back();
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(FinanceCategory $financeCategory)
+    public function destroy(Request $request, $recordId)
     {
-        //
+        try {
+            // Delete record
+            FinanceCategory::find($recordId)->delete();
+
+            $request->session()->flash('toast', ['error', 'The category ' . '"' . $request->name. '"' . ' has been deleted!', time()]);
+
+            return redirect()->back();
+
+        } catch (\Throwable $th) {
+
+            $request->session()->flash('toast', ['warning', $th->getMessage(), time()]);
+
+            return redirect()->back();
+        }
     }
 }
